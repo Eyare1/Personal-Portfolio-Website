7bc489ac59160864d2ba1e73115f303a3049d8db
Eyare <155492546+Eyare1@users.noreply.github.com>
2024-02-11 18:07:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -46,6 +46,103 @@ window.onscroll = () => {
 
 }
 
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+let contactForm = document.getElementById("contactForm");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> phone Number: ${phone.value}<br> Message: ${mess.value}`;
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "eyarewebsite@gmail.com",
+        Password: "E63E5A625D8058A2A707886B5FBDEBCC494B",
+        To: 'eyarewebsite@gmail.com',
+        From: "eyarewebsite@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "success!",
+                    text: "Message sent successfully!",
+                    icon: "success"
+                  });
+            }
+        }
+    );    
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+
+function checkEmail () {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+    
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "Enter a valid email address";
+        }
+        else {
+            errorTxtEmail.innerText = "Email Address can't be blank";
+        }
+    }    
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+    
+    if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.
+    contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+        
+        sendEmail();
+        contactForm.reset();
+        return false;
+    }
+});
+
+
 function myFunction() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
